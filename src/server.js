51198fa64c46ea30f091e63d3015a795b8898628
Michael Z <michaelzwierzanski@gmail.com>
2023-04-11 16:20:14 +0100

    add get route

--- src/server.js
+++ src/server.js
@@ -4,4 +4,8 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello world from the get request!");
+});
+
 app.listen(5002, () => console.log("Server is listening"));
