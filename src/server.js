d7ae09974c12f360a81b9936234b93b69f3769ec
Michael Z <michaelzwierzanski@gmail.com>
2023-04-11 16:23:52 +0100

    add post route

--- src/server.js
+++ src/server.js
@@ -8,4 +8,9 @@ app.get("/", (req, res) => {
   res.send("Hello world from the get request!");
 });
 
+//this is a comment
+app.post("/", (req, res) => {
+  res.send("Hello from the post request");
+});
+
 app.listen(5002, () => console.log("Server is listening"));
